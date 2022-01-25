b94e9ba09167be7240a8ea830e978b315f49d8f7
changgun <pcg0527@naver.com>
2022-01-26 06:22:45 +0900

    feat: #S06P12D206-61 [FE] 랜딩페이지 UI 구성
    랜딩페이지 UI구성하였다. 기존에 있던 회원가입/로그인 모달창과
    연결했으며, 로직이 바뀐 점은 없다. 다만, 현재 모든 컴포넌트가
    하나의 디렉토리에서 관리되고 있기 때문에 분리가 필요하다.

--- frontend/src/components/LandingPage/Login.js
+++ frontend/src/components/LandingPage/Login.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
-import { propTypes } from "react-bootstrap/esm/Image";
-import { useEffect, useState } from "react";
-import './Login.css';
+import "./Login.css";
 import { type } from "@testing-library/user-event/dist/type";
 
-
-
 const Login = (props) => {
-//   const [AlphaNum, setAlphaNum] = useState("");
-//   const IsAlphaNume=(e)=>{
-//   const curValue=e.currentTarget.value;
-//   const notNum=/[^a-z0-9~!@#$%";'^,&*()_+|</>=>`?:{[\}]/gi;
-// // 숫자, 대소문자, 특수문자 빼곤 안됨,https://grownfresh.tistory.com/112 일단 낼참고
-//   setAlphaNum(curValue.replace(notNum,''))
-// };
+  //   const [AlphaNum, setAlphaNum] = useState("");
+  //   const IsAlphaNume=(e)=>{
+  //   const curValue=e.currentTarget.value;
+  //   const notNum=/[^a-z0-9~!@#$%";'^,&*()_+|</>=>`?:{[\}]/gi;
+  // // 숫자, 대소문자, 특수문자 빼곤 안됨,https://grownfresh.tistory.com/112 일단 낼참고
+  //   setAlphaNum(curValue.replace(notNum,''))
+  // };
   return (
     <div className="login-form">
       <h3>Login</h3>
@@ -30,32 +26,34 @@ const Login = (props) => {
           {/* <Form.Label>Email address</Form.Label> */}
           <Form.Control
             id="input"
-            type="email" 
+            type="email"
             placeholder="Enter Email"
             required
-             />
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           {/* <Form.Label id="label">PASSWORD</Form.Label> */}
-          <Form.Control id="input"
-            
+          <Form.Control
+            id="input"
             type="password"
             placeholder="PASSWORD"
             required
-             />
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        
+
         {/* <Button variant="primary" type="submit"> */}
         <Button className="login-button" type="submit">
           Login
-        </Button>  
+        </Button>
       </Form>
       <div className="caption">
-        <a href="#" onClick={props.signupButtonClicked}>아직 회원이 아니신가요?</a>
+        <a href="#" onClick={props.signupButtonClicked}>
+          아직 회원이 아니신가요?
+        </a>
         {/* <Button onClick={props.signupButtonClicked}>회원가입</Button> */}
       </div>
     </div>
