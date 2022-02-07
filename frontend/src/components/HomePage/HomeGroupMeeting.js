8a0a1ac112922f8887eb0bf939e16aad904ee840
6th_gumi_3_chs7720 <chs7720@naver.com>
2022-02-07 14:09:53 +0900

    feat: #S06P12D206-95 [FE] 경매실 생성 / 페이지 UI / 오픈비두 추가

--- frontend/src/components/HomePage/HomeGroupMeeting.js
+++ frontend/src/components/HomePage/HomeGroupMeeting.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Room from "../RoomPage/Room";
 
 const HomeGroupMeeting = () => {
   return (
     <div>
       <h1>이게그룹미팅</h1>
+      <p>vs코드에서 터미널 하나 더 키고</p>
+      <p>docker run -p 4443:4443 --rm -e OPENVIDU_SECRET=MY_SECRET openvidu/openvidu-server-kms:2.20.0</p>
+      <p>복붙</p>
+      <Room />
     </div>
   );
 };
