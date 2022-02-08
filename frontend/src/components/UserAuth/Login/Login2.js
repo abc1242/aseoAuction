abf19c5e8a089e26173a20efb1ba97e58c6cd420
changgun <pcg0527@naver.com>
2022-02-08 15:04:27 +0900

    refactor: #S06P12D206-123 [FE] 회원가입 / 모달 삭제, UI 구성 / 회원가입, 로그인 모두 변경
    useReducer와 useEffect로 관리하던 상태를 useRef로 바꾸어 코드를 간단하게
    리팩토링했다. 로그인과 함께 진행했으며, 이슈번호는 footer를 참고하면
    된다.
    
    closes: #S06P12D206-122

--- frontend/src/components/UserAuth/Login/Login2.js
+++ frontend/src/components/UserAuth/Login/Login2.js
@@ -1,7 +1,76 @@
-import React from "react";
+import classes from "./Login2.module.css";
+import logo from "../../../images/logo.png";
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login2 = () => {
-  return <div>Login2</div>;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [error, setError] = useState();
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // validation
+    if (emailRef.current.value.trim().length === 0) {
+      setError("이메일을 입력하세요");
+      return;
+    }
+    if (passwordRef.current.value.trim().length === 0) {
+      setError("비밀번호를 입력하세요");
+      return;
+    }
+
+    const userInfo = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+  };
+
+  return (
+    <div className={`container-small`}>
+      <section className={`${classes.authBox} flexbox`}>
+        <Link to="/">
+          <div className={` ${classes.imgBox}`}>
+            <img className={classes.logoImg} src={logo} alt="로고" />
+          </div>
+        </Link>
+        <form className={classes.formBox}>
+          <div className={classes.control}>
+            <input
+              ref={emailRef}
+              className={classes.input}
+              placeholder="email"
+            />
+          </div>
+          <div className={classes.control}>
+            <input
+              ref={passwordRef}
+              className={classes.input}
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div className={classes.alertMessage}>{error}</div>
+          <button onClick={onSubmitHandler} className={classes.button}>
+            로그인
+          </button>
+        </form>
+        <div className={classes.helperBox}>
+          <Link className={`${classes.helperContent} ${classes.alignLeft}`}>
+            비밀번호 찾기
+          </Link>
+          <div className={`${classes.helperContent}`}>|</div>
+          <Link
+            to="/signup"
+            className={`${classes.helperContent} ${classes.alignLeft}`}
+          >
+            회원 가입
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Login2;
--- frontend/src/components/UserAuth/Signup/Signup2.js
+++ frontend/src/components/UserAuth/Signup/Signup2.js
@@ -1,7 +1,95 @@
-import React from "react";
+import classes from "./Signup2.module.css";
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../images/logo.png";
 
 const Signup2 = () => {
-  return <div>Signup2</div>;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordCheckRef = useRef();
+  const nicknameRef = useRef();
+  const [error, setError] = useState();
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // validation
+    if (!emailRef.current.value.includes("@")) {
+      setError("이메일을 정확히 입력해 주세요");
+      return;
+    }
+    if (nicknameRef.current.value.trim().length === 0) {
+      setError("닉네임을 입력해 주세요");
+      return;
+    }
+    if (passwordRef.current.value.trim().length === 0) {
+      setError("비밀번호를 입력해주세요");
+      return;
+    }
+    if (passwordCheckRef.current.value !== passwordRef.current.value) {
+      setError("비밀번호 재입력이 잘못되었습니다");
+      return;
+    }
+
+    const userInfo = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      nickname: nicknameRef.current.value,
+    };
+  };
+
+  return (
+    <div className={`container-small`}>
+      <section className={`${classes.authBox} flexbox`}>
+        <Link to="/">
+          <div className={` ${classes.imgBox}`}>
+            <img className={classes.logoImg} src={logo} alt="로고" />
+          </div>
+        </Link>
+        <form className={classes.formBox}>
+          <div className={classes.control}>
+            <input
+              ref={emailRef}
+              className={classes.input}
+              placeholder="email"
+            />
+          </div>
+          <div className={classes.control}>
+            <input
+              ref={nicknameRef}
+              className={classes.input}
+              placeholder="nickname"
+            />
+          </div>
+          <div className={classes.control}>
+            <input
+              ref={passwordRef}
+              className={classes.input}
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div className={classes.control}>
+            <input
+              ref={passwordCheckRef}
+              className={classes.input}
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div className={classes.alertMessage}>{error}</div>
+          <button onClick={onSubmitHandler} className={classes.button}>
+            회원가입
+          </button>
+        </form>
+        <div className={classes.helperBox}>
+          <Link to="/login" className={classes.helperContent}>
+            회원이신가요?
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Signup2;
