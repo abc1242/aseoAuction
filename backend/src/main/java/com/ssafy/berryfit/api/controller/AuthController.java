8a5a5ddabb97f86049fac232ebbdc45e035cdf54
kisung <abc1242@naver.com>
2022-02-10 03:52:11 +0900

    fix : 이메일 인증안된 아이디 로그인시 토큰발행 수정

--- backend/src/main/java/com/ssafy/berryfit/api/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/berryfit/api/controller/AuthController.java
@@ -60,6 +60,15 @@ public class AuthController {
 		String password = loginInfo.getPassword();
 		System.out.println("로그인 시도");
 		User user = userService.getUserByEmail(email);
+		
+		
+		//이메일 인증안하고 로그인 하면 돌려보내기
+		if(!user.isAuthStatus()) {
+			System.out.println(user.isAuthStatus()+"0d이다");
+			return ResponseEntity.status(401).body(UserLoginPostRes.of(403, "이메일 인증 해주세요", null));
+		}
+		
+		
 		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
 		if(passwordEncoder.matches(password, user.getPassword())) {
 			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
@@ -67,7 +76,7 @@ public class AuthController {
 		}
 	
 		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "비밀번호가 틀렸습니다.", null));
 	}
 	
 	@PostMapping("/logout")
