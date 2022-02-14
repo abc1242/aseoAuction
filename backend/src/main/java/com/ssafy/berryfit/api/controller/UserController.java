976c630d682604bb49df378120255a8650f502c6
kisung <abc1242@naver.com>
2022-02-14 14:26:36 +0900

    fix : 마이페이지조회 매핑오류 수정

--- backend/src/main/java/com/ssafy/berryfit/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/berryfit/api/controller/UserController.java
@@ -75,7 +75,7 @@ public class UserController {
 	}
 
 	
-	@PutMapping("/mypage")
+	@PostMapping("/mypage")
 	public ResponseEntity getmypage(@RequestBody UserLoginPostReq loginreq) {
 		System.out.println("마이페이지호출");
 		//req 정보
