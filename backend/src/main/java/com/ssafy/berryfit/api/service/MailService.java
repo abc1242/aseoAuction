24c0099d3c01fe1aca353bc92d2bd50438913de1
kisung <abc1242@naver.com>
2022-02-18 08:57:59 +0900

    fix : 회원가입 메일 인증오류 수정

--- backend/src/main/java/com/ssafy/berryfit/api/service/MailService.java
+++ backend/src/main/java/com/ssafy/berryfit/api/service/MailService.java
@@ -68,7 +68,7 @@ public class MailService {
                 "		감사합니다."																																															+
                 "	</p>"																																																	+
                 "	<a style=\"color: #FFF; text-decoration: none; text-align: center;\""																																	+
-                "	href=\"" + serverAddress + "/user/signup/confirm?email=" + email + "&authKey=" + authKey + "\" target=\"_blank\">"														+
+                "	href=\"" + serverAddress + "/api/user/signup/confirm?email=" + email + "&authKey=" + authKey + "\" target=\"_blank\">"														+
                 "<div style=\"margin: 0 auto;\">" +
                 "		<p"																																																	+
                 "			style=\"display: inline-block; width: 210px; height: 45px; margin: 30px 5px 50px; background: #FF0066; line-height: 45px; vertical-align: middle; font-size: 16px;\">"							+
